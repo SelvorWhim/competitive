0987589ee5fa7aeee0c714a254b2a96f5a200af3
Carmeverre <Carmeverre@gmail.com>
2017-10-06 23:10:08 +0300

    reorg

---
+++ ACM-ICPC(cpp)/template.cpp
@@ -0,0 +1,19 @@
+// TODO: use richer template like partner's
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+//#include <set>
+//#include <vector>
+//#include <map>
+//#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	//int n,m;
+	//cin >> n >> m; // read 2
+	//cout << "NO" << endl; // print 2. endl is like a linebreak
+	return 0;
+}
