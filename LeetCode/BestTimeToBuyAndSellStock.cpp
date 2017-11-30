12c079be718345ed2614fa390b28dbff42715be4
Carmeverre <Carmeverre@gmail.com>
2017-11-30 21:32:52 +0200

    LeetCode 39.2
    "Best Time to Buy and Sell Stock" (non-optimized v2)

--- LeetCode/BestTimeToBuyAndSellStock.cpp
+++ LeetCode/BestTimeToBuyAndSellStock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    // non-optimized version - O(n) time, O(n) space. Pretty sure I can change it to use constant extra space.
+	/*
+    // non-optimized v1 - O(n) time, O(n) space. Pretty sure I can change it to use constant extra space.
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         if (n < 2) return 0;
@@ -14,6 +15,21 @@ public:
             int profit = maxAfter[i] - prices[i];
             if (profit > maxProfit)
                 maxProfit = profit;
+        }
+        return maxProfit;
+    }
+	*/
+	
+	// non-optimized v2 - same complexity, shorter with joined loops
+	int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n < 2) return 0;
+        int maxAfter[n];
+        maxAfter[n-1] = 0;
+        int maxProfit = 0;
+        for (int i = n-2; i >= 0; i--) {
+            maxAfter[i] = max(maxAfter[i+1], prices[i+1]);
+            maxProfit = max(maxProfit, maxAfter[i] - prices[i]);
         }
         return maxProfit;
     }
