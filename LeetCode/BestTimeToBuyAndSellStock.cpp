d0ca3a7dd899a053fa9e505e4a71c0e7b53f32e6
Carmeverre <Carmeverre@gmail.com>
2017-11-30 21:40:45 +0200

    LeetCode 39.3
    "Best Time to Buy and Sell Stock" (optimized)

--- LeetCode/BestTimeToBuyAndSellStock.cpp
+++ LeetCode/BestTimeToBuyAndSellStock.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+	// idea: iterate backwards over prices, remember what the maximum sell price is later and so find the maximum profit if the buying price is the current. Keep track of the max of these values, giving maximum profit over the list.
+	
 	/*
     // non-optimized v1 - O(n) time, O(n) space. Pretty sure I can change it to use constant extra space.
     int maxProfit(vector<int>& prices) {
@@ -20,6 +22,7 @@ public:
     }
 	*/
 	
+	/*
 	// non-optimized v2 - same complexity, shorter with joined loops
 	int maxProfit(vector<int>& prices) {
         int n = prices.size();
@@ -30,6 +33,19 @@ public:
         for (int i = n-2; i >= 0; i--) {
             maxAfter[i] = max(maxAfter[i+1], prices[i+1]);
             maxProfit = max(maxProfit, maxAfter[i] - prices[i]);
+        }
+        return maxProfit;
+    }
+	*/
+	
+	// optimized version - O(n) time, O(1) space
+	int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+        int maxAfterNext = 0;
+        int maxProfit = 0;
+        for (int i = prices.size()-2; i >= 0; i--) {
+            maxAfterNext = max(maxAfterNext, prices[i+1]);
+            maxProfit = max(maxProfit, maxAfterNext - prices[i]);
         }
         return maxProfit;
     }
