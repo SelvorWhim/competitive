fbe0dc8e592ceb1d991095a54d27109e487b8a99
Carmeverre <Carmeverre@gmail.com>
2017-12-02 21:18:18 +0200

    LeetCode 41
    "Valid Anagram" (Python one-liner)

---
+++ LeetCode/ValidAnagram.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    # one-liner (besides import)
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return Counter(s) == Counter(t)
