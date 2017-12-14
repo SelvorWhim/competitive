bf8afd3b3a28ab72b096fdb54964af46ce07f75f
Carmeverre <Carmeverre@gmail.com>
2017-12-14 13:42:07 +0200

    LeetCode 52.2
    "Find Smallest Letter Greater Than Target" - optimized solution (asymptotically, but probably no better in this case)

--- LeetCode/FindSmallestLetterGreaterThanTarget.py
+++ LeetCode/FindSmallestLetterGreaterThanTarget.py
@@ -1,13 +1,19 @@
 # assumes all letters are lowercase alphabetical (all uppercase should work too, but not mixed or any other characters or non-characters)
 
+from bisect import bisect # binary search
+
+'''
 ALPHABET_LEN = ord('z') - ord('a') + 1
 
 def dist_to_letter(target, letter):
     if target == letter:
         return ALPHABET_LEN # because it must be strictly "bigger", the target itself is furthest
     return (ord(letter) - ord(target) + ALPHABET_LEN) % ALPHABET_LEN
+'''
 
 class Solution:
+    # non-optimized v1 - does not take pre-sort into account. O(n) time, O(1) space. Of course, if letters are non-repeating, n<=26 so it's no big deal. In fact, overhead for a more asymptotically optimized solution might make actual complexity worse
+    '''
     def nextGreatestLetter(self, letters, target):
         """
         :type letters: List[str]
@@ -22,3 +28,10 @@ class Solution:
                 best_d = d
                 best_l = l
         return best_l
+    '''
+    
+    # optimized v2 - O(log(n)) time, O(1) space
+    # idea: letters are sorted alphabetically, but due to wraparound distance is not quite. However, it would be enough to find the closest letter and take the next one if it's smaller or equal, wrapping around if necessary
+    def nextGreatestLetter(self, letters, target):
+        i = bisect(letters, target) # this variant returns index just after any appearance of target or anything smaller - perfect. No option for a key function but default comparator works on characters as necessary
+        return letters[0] if i >= len(letters) else letters[i]
