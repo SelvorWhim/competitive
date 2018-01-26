52e556fa8e4c4e279312f9547a3fb9c60b1c1077
Carmeverre <Carmeverre@gmail.com>
2018-01-26 23:41:36 +0200

    LeetCode 89
    "Remove Element"

---
+++ LeetCode/RemoveElement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int lf = -1; // last free spot
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val && (lf == -1)) {
+                lf = i;
+            }
+            else if (nums[i] != val && (lf != -1)) {
+                nums[lf] = nums[i];
+                lf++;
+            }
+        }
+        if (lf == -1) return nums.size(); // none were val
+        for (int i = 0; i < nums.size() - lf; i++) {
+            nums.pop_back();
+        }
+        return lf;
+    }
+};
