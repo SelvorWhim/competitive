088ea463e0248967618933c35ceada61415ec574
Carmeverre <Carmeverre@gmail.com>
2018-02-21 17:21:16 +0200

    LeetCode 110.1
    "Longest Harmonious Subsequence" - fixed

--- LeetCode/LongestHarmoniousSubsequence.cpp
+++ LeetCode/LongestHarmoniousSubsequence.cpp
@@ -7,9 +7,8 @@ class Solution:
         :rtype: int
         """
         c = Counter(nums) # c[n] counts the number of apperances of n
-        for n,cn in c.most_common():
-            if c[n-1] > 0 and c[n-1] >= c[n+1]:
-                return cn + c[n-1]
+        lhs = 0
+        for n in c:
             if c[n+1] > 0:
-                return cn + c[n+1]
-        return 0
+                lhs = max(lhs, c[n] + c[n+1])
+        return lhs
