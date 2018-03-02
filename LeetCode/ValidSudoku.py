ccf18d64c0b1819ea347a353e95e2454d470a02e
Carmeverre <Carmeverre@gmail.com>
2018-03-02 14:39:44 +0200

    LeetCode 118.1.2
    "Valid Sudoku" - even more DRY

--- LeetCode/ValidSudoku.py
+++ LeetCode/ValidSudoku.py
@@ -13,21 +13,15 @@ class Solution:
         :type board: List[List[str]]
         :rtype: bool
         """
-        # validate rows and columns
-        for i in range(9):
+        for i in range(9): # row/column/block index
             sr = set() # seen set for row being validated
             sc = set() # seen set for column being validated
-            for j in range(9):
+            sb = set() # seen set for 3x3 block being validated
+            for j in range(9): # index within row/column/block
                 if not validCell(board[i][j], sr):
                     return False
                 if not validCell(board[j][i], sc):
                     return False
-        # validate (relevant) 3x3 blocks
-        for i in range(3):
-            for j in range(3):
-                sb = set() # seen set for block being validated
-                for k in range(3):
-                    for l in range(3): # never thought I'd have reason to use a 4-level loop (on an object of depth less than 4), yet here I am. It could have been 2 levels but this is more convenient
-                        if not validCell(board[i*3+k][j*3+l], sb):
-                            return False
+                if not validCell(board[3*(i//3) + (j//3)][3*(i%3) + (j%3)], sb): # translation of block index / in-block index to row/column indexes
+                    return False
         return True
