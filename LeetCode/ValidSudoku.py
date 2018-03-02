daee43f642ec5bf217b45bc29acda1e8fc10a644
Carmeverre <Carmeverre@gmail.com>
2018-03-02 14:29:41 +0200

    LeetCode 117.1.1
    "Valid Sudoku" - more DRY

--- LeetCode/ValidSudoku.py
+++ LeetCode/ValidSudoku.py
@@ -1,5 +1,12 @@
 # assumption: board is of valid size, and contains only single character strings with digits or '.'
 
+def validCell(val, seen):
+    if val != '.' and val in seen:
+        return False
+    else:
+        seen.add(val)
+        return True
+
 class Solution:
     def isValidSudoku(self, board):
         """
@@ -11,25 +18,16 @@ class Solution:
             sr = set() # seen set for row being validated
             sc = set() # seen set for column being validated
             for j in range(9):
-                curr = board[i][j]
-                if curr != '.' and curr in sr:
+                if not validCell(board[i][j], sr):
                     return False
-                else:
-                    sr.add(curr)
-                curr = board[j][i]
-                if curr != '.' and curr in sc:
+                if not validCell(board[j][i], sc):
                     return False
-                else:
-                    sc.add(curr)
         # validate (relevant) 3x3 blocks
         for i in range(3):
             for j in range(3):
                 sb = set() # seen set for block being validated
                 for k in range(3):
                     for l in range(3): # never thought I'd have reason to use a 4-level loop (on an object of depth less than 4), yet here I am. It could have been 2 levels but this is more convenient
-                        curr = board[i*3+k][j*3+l]
-                        if curr != '.' and curr in sb:
+                        if not validCell(board[i*3+k][j*3+l], sb):
                             return False
-                        else:
-                            sb.add(curr)
         return True
