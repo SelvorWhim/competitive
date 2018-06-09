2b85d832909915374d757e1d24833816eebf623b
Carmeverre <Carmeverre@gmail.com>
2018-06-09 23:38:51 +0300

    Codewars: Playing with passphrases
    more readable and DRY code

--- Codewars/PlayingWithPassphrases.py
+++ Codewars/PlayingWithPassphrases.py
@@ -4,15 +4,14 @@ class PassphraseMapper(object):
     
     def __getitem__(self, c):
         c = chr(c)
-        if c.islower():
-            return chr(ord('a')+(ord(c)-ord('a')+self.n)%26) # circular shift for lowercase letters
-        if c.isupper():
-            return chr(ord('A')+(ord(c)-ord('A')+self.n)%26) # circular shift for uppercase letters
-        if c.isdigit():
-            return str(9 - int(c)) # complement to 9
-        return c
+        if c.isalpha(): # circular shift for letters
+            a = ord('a') if c.islower() else ord('A')
+            return chr(a + ((ord(c) - a + self.n) % 26))
+        if c.isdigit(): # complement to 9 for digits
+            return str(9 - int(c))
+        return c # leave the rest as is
 
 def play_pass(s, n):
-    step3 = s.translate(PassphraseMapper(n))
-    step4 = "".join([step3[i].upper() if i%2==0 else step3[i].lower() for i in range(len(step3))])
-    return step4[::-1]
+    step3 = s.translate(PassphraseMapper(n)) # steps 1-3 (out of context character translation)
+    step4 = "".join([step3[i].upper() if (i % 2 == 0) else step3[i].lower() for i in range(len(step3))]) # step 4 (character capitalization by index)
+    return step4[::-1] # step 5 (reversal)
