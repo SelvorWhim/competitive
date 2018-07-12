cf5487468d4f36fde99138644c501c77b3259d81
Carmeverre <Carmeverre@gmail.com>
2018-07-12 23:40:04 +0300

    Codewars: Minimum unfairness of a list/array
    concision

--- LeetCode/MinimumUnfairnessOfAListArray.py
+++ LeetCode/MinimumUnfairnessOfAListArray.py
@@ -6,7 +6,4 @@ def min_unfairness(arr,k):
     if k==0 or len(arr)==0:
         return 0
     arr.sort() # mutates. Non mutating version will take extra space
-    min_unf = arr[-1] - arr[0] # max possible value here
-    for i in range(len(arr)-(k-1)):
-        min_unf = min(min_unf, arr[i+(k-1)] - arr[i])
-    return min_unf
+    return min((arr[i+(k-1)] - arr[i]) for i in range(len(arr)-(k-1)))
