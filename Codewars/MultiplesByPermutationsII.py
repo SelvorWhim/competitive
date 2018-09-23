340134fe9e99f92aa2bf5461ab02b658edf8c81b
Carmeverre <Carmeverre@gmail.com>
2018-09-23 09:34:51 +0300

    Codewars: Multiples By Permutations II (optimization)
    a solution exactly 9 times faster WAS fast enough to pass

--- Codewars/MultiplesByPermutationsII.py
+++ Codewars/MultiplesByPermutationsII.py
@@ -1,8 +1,11 @@
 from collections import Counter
 
+# idea: the difference between k*n and (k+1)*n is n
+# for them to have the same digits they must have the same digit sum
+# so n must have a digit sum of 0 (mod 9) - n must be divisible by 9
 def find_lowest_int(k):
-    n = 1
+    n = 9
     while True:
         if Counter(str(k*n)) == Counter(str((k+1)*n)):
             return n
-        n += 1
+        n += 9
