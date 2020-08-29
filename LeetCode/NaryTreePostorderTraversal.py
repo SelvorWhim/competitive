04b8e8085003bb26f8423a05f4b5b99f49b59428
Selvor Whim <Selvor.Whim@gmail.com>
2020-08-29 10:45:50 +0300

    LeetCode: 590. N-ary Tree Postorder Traversal

---
+++ LeetCode/NaryTreePostorderTraversal.py
@@ -0,0 +1,13 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        return [val for child in root.children for val in self.postorder(child)] + [root.val]
