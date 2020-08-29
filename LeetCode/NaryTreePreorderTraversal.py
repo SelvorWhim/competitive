15bc575d1353037840cd5a9e9cf5def377989599
Selvor Whim <Selvor.Whim@gmail.com>
2020-08-29 10:49:30 +0300

    LeetCode: 589. N-ary Tree Preorder Traversal

---
+++ LeetCode/NaryTreePreorderTraversal.py
@@ -0,0 +1,13 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        return [root.val] + [val for child in root.children for val in self.preorder(child)]
