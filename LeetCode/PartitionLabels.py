ec74c30184826cb86182a02846e727bc14b11fe2
Selvor Whim <Selvor.Whim@gmail.com>
2020-09-04 23:56:50 +0300

    LeetCode: 763. Partition Labels: fix interval merge algorithm

--- LeetCode/PartitionLabels.py
+++ LeetCode/PartitionLabels.py
@@ -3,6 +3,27 @@
 
 from collections import OrderedDict
 
+# adapted from: https://www.geeksforgeeks.org/merging-intervals/
+# assumes intervals are sorted by interval start, and all values are non-negative
+# (in this problem they represent indexes, so they are)
+def merge_overlapping_intervals(intervals): 
+    ret = [] 
+    curr_start = -1
+    curr_end = -1
+    for i in range(len(intervals)): 
+        curr_interval = intervals[i] 
+        if curr_interval[0] > curr_end: 
+            if i > 0: 
+                ret.append([curr_start, curr_end]) 
+            curr_end = curr_interval[1] 
+            curr_start = curr_interval[0] 
+        else: 
+            if curr_interval[1] >= curr_end: 
+                curr_end = curr_interval[1] 
+    if curr_end >= 0 and [curr_start, curr_end] not in ret: 
+        ret.append([curr_start, curr_end])
+    return ret
+
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
         # construct intervals:
@@ -16,10 +37,6 @@ class Solution:
         
         # merge intervals:
         intervals = list(letter_intervals.values())
-        n = len(intervals)
-        for i in range(n - 1, 0, -1): # iterate indexes backward se we can merge as we go
-            if intervals[i-1][1] >= intervals[i][0]: # if overlap, merge to first in pair and delete 2nd
-                intervals[i-1] = [intervals[i-1][0], max(intervals[i-1][1], intervals[i][1])]
-                del intervals[i]
-        intervals_lens = [(interval[1] - interval[0] + 1) for interval in intervals]
+        merged_intervals = merge_overlapping_intervals(intervals)
+        intervals_lens = [(interval[1] - interval[0] + 1) for interval in merged_intervals]
         return intervals_lens
