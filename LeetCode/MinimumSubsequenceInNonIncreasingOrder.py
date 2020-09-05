3a0f33d2d9cb760f89602a2a88f8284d1ddfc2ed
Selvor Whim <Selvor.Whim@gmail.com>
2020-09-06 00:02:59 +0300

    LeetCode: 1403. Minimum Subsequence in Non-Increasing Order - as intended
    now add the solution for the problem as intended

--- LeetCode/MinimumSubsequenceInNonIncreasingOrder.py
+++ LeetCode/MinimumSubsequenceInNonIncreasingOrder.py
@@ -1,6 +1,18 @@
 # idea: order by size and take largest elements until the sum becomes > sum of remaining
-# but keep track of original order so subsequence can be returned in original order
 class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums = sorted(nums, reverse=True)
+        total_sum = sum(nums)
+        running_sum = 0
+        subseq_len = 0 # how many biggest members we'll need to take before sum is greater than the rest
+        for x in nums:
+            running_sum += x
+            subseq_len += 1
+            if running_sum > (total_sum - running_sum):
+                break
+        return nums[:subseq_len]
+
+    # in this variant (not relevant for the problem as described) we keep track of original order so subsequence can be returned in original order
     def minSubsequenceInOriginalOrder(self, nums: List[int]) -> List[int]:
         total_sum = sum(nums)
         sorted_nums = sorted(enumerate(nums), key=lambda x: x[1], reverse=True) # preserving original order in 1st index
