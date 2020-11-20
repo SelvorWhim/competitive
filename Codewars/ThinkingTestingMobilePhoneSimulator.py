bd1f4f17f0e0986e24edff7bd46bddc9a10d1321
Selvor Whim <Selvor.Whim@gmail.com>
2020-11-20 13:28:28 +0200

    Codewars: Thinking & Testing : Mobile phone simulator
    fixed

--- Codewars/ThinkingTestingMobilePhoneSimulator.py
+++ Codewars/ThinkingTestingMobilePhoneSimulator.py
@@ -1,22 +1,14 @@
 # assumes incomingcall, connect and hangup will be called in sequence
-# partial solution: convert_contacts doesn't work for full tests because some of them change contacts after creating the Phone object
 
 class Phone:
     caller = ""
     ring = ""
     screen = ""
     microphone = ""
-    contact_dict = {} # maps number to tuple of name and ring
-    
-    def __init__(self):
-        self.convert_contacts(contacts)
-    
-    def convert_contacts(self, unconverted_contacts):
-        for contact in unconverted_contacts:
-            self.contact_dict[contact['number']] = (contact['name'], contact['ring'])
 
     def incomingcall(self, num_calling):
-        self.caller, self.ring = self.contact_dict.get(num_calling, ("stranger", "Di Da Di"))
+        # doing this lookup every time is inefficient, but necessary because contacts changes during tests
+        self.caller, self.ring = next(((contact["name"], contact["ring"]) for contact in contacts if contact["number"] == num_calling), ("stranger", "Di Da Di"))
         self.screen = f"Call: {self.caller}\nNumber: {num_calling}"
     
     def connect(self):
