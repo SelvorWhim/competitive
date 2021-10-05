0f73a23d00ac868cd9d16b00ae169173bdafa48a
Selvor Whim <Selvor.Whim@gmail.com>
2021-10-05 10:25:36 +0300

    typo fix

--- LeetCode/ClimbingStairs.cpp
+++ LeetCode/ClimbingStairs.cpp
@@ -5,7 +5,7 @@ public:
     int climbStairs(int n) {
         vector<int> counts {1,1}; // 1 way to split a 1, and 1 way to split a 0 just to provide a base case that works with the rest (if n is positive, it won't come up in and of itself)
         for (int i = 2; i <= n; i++) {
-            counts.push_back(counts[i-2] + counts[i-1];);
+            counts.push_back(counts[i-2] + counts[i-1]);
         }
         return counts[n];
     }
