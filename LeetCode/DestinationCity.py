ccffe915ab08e175056a8a69199778bede1b4698
Selvor Whim <Selvor.Whim@gmail.com>
2023-12-15 10:24:40 +0200

    LeetCode: 1436. Destination City
    (accidentally re-did this one, got a lower-memory solution)

--- LeetCode/DestinationCity.py
+++ LeetCode/DestinationCity.py
@@ -1,11 +1,16 @@
 # given there's exactly one destination city, just find one that appears in the list but never first in a pair
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        not_destination = set() # cities with outgoing paths
-        destination_candidate = set()
-        for from_city, to_city in paths:
-            not_destination.add(from_city)
-            destination_candidate.discard(from_city)
-            if to_city not in not_destination:
-                destination_candidate.add(to_city)
-        return destination_candidate.pop() # by assumption, there should be exactly one
+        destinations_that_are_not_sources = set()
+        sources_that_are_not_destinations = set()
+        for source, destination in paths:
+            if source in destinations_that_are_not_sources:
+                destinations_that_are_not_sources.remove(source)
+            else:
+                sources_that_are_not_destinations.add(source)
+            if destination in sources_that_are_not_destinations:
+                sources_that_are_not_destinations.remove(destination)
+            else:
+                destinations_that_are_not_sources.add(destination)
+        assert len(destinations_that_are_not_sources) == 1
+        return next(iter(destinations_that_are_not_sources))
