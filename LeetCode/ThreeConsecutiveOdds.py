7ea82259f99934778189e4cc7baed11cb167c4e1
Selvor Whim <Selvor.Whim@gmail.com>
2024-07-01 22:27:08 +0300

    LeetCode: 1550. Three Consecutive Odds - fix

--- LeetCode/ThreeConsecutiveOdds.py
+++ LeetCode/ThreeConsecutiveOdds.py
@@ -1,11 +1,11 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        consecurive_odds = 0
+        consecutive_odds = 0
         for n in arr:
             if n % 2 == 1:
-                consecurive_odds += 1
-                if consecurive_odds == 3:
+                consecutive_odds += 1
+                if consecutive_odds >= 3:
                     return True
             else:
-                consecurive_odds = 0
+                consecutive_odds = 0
         return False
